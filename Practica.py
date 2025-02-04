453818146c67a6018f2cf60a16cecf326276629f
MomAligheri <dega2005alej@gmail.com>
2025-02-04 07:55:03 -0600

    New Update

--- Practica.py
+++ Practica.py
@@ -1,19 +1,17 @@
-class figura:
-    Lados = 4
-    Medidalados = 5
-    def area(Lados,Medidalados):
-        if Lados = 4:
+class Figura:
+    def area(Lados, Medidalados):
+        if Lados == 4:
             print("Es un cuadrado")
             Area = Medidalados * Medidalados
-            print ("El Area es: ", Medidalados)
-
-        elif Lados = 3:
+            print("El Area es:", Area)
+        elif Lados == 3:
             print("Es un triangulo")
-
-    def perimetro(Lados,Medidalados):
-        if Lados = 4:
+    
+    def perimetro(Lados, Medidalados):
+        if Lados == 4:
             perimetro = Medidalados * 4
             print("El perimetro es:", perimetro)
 
+Figura.area(4, 5)
+Figura.perimetro(4, 5)
 
-    
