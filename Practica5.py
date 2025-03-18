1c53f6e8214ce3ec0a45c28d7a1db34db6664668
MomAligheri <dega2005alej@gmail.com>
2025-03-18 07:56:04 -0600

    Update

--- Practica5.py
+++ Practica5.py
@@ -1,3 +1,9 @@
-#Atravez de las llaves acceder al valor de esa llave
-#Diccionarios
-#var:valor
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    def saludar(self):
+        return f"Hola, soy {self.nombre} y tengo {self.edad} años"
+# Crear un objeto
+juan = Persona("Juan", 30)
+print(juan.saludar()) # Output: Hola, soy Juan y tengo 30 años
