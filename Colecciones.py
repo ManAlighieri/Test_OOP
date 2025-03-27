bcc98b385eb8d5301ada284e015761d9c0fd3e81
MomAligheri <dega2005alej@gmail.com>
2025-03-27 08:49:47 -0600

    Nueva Practica

---
+++ Colecciones.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy import array
+
+lista1 = [1,1.1,3,"Pepe","Serch"]
+lista2 = np.array([1,2,3,4,5])
+
+tupla1 = tuple(lista1)
+conj1 = set(lista1)
+
+tupla2 = tuple(lista2)
+conj2 = set(lista2)
+diccionario = {"1":1,
+               "2":True,
+               "3": 3.3,
+               "4": "4",
+               "5":{1}}
+print(tupla2)
