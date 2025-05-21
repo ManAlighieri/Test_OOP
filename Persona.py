59d5fa72ceab2636637562d21a756017394e9ed5
MomAligheri <dega2005alej@gmail.com>
2025-05-21 17:04:06 -0600

    Macarena

--- Persona.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
-    def saludar(self):
-        return f"Hola, soy {self.nombre} y tengo {self.edad} años"
-# Crear un objeto
-juan = Persona("Juan", 30)
-print(juan.saludar()) # Output: Hola, soy Juan y tengo 30 años
