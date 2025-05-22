6b2a1589548ebfb7335c6020d1f9ef91672198b8
MomAligheri <dega2005alej@gmail.com>
2025-05-22 08:55:49 -0600

    FINAL!

--- Practica 13/Diccionarios.py
+++ Practica 13/Diccionarios.py
@@ -1,21 +1,27 @@
-#Diccionarios
 Dic = {"x": "equis", "y": "ye", "D": "De"}
-print(Dic['x'])               
-print(Dic.get('x'))           
-print(Dic.get('z'))          
+Dic2 = dict(x="equis", y="ye", D="DC")
+
+print(Dic['x'])
+print(Dic.get('x'))
+print(Dic.get('z'))
 
 Dic['x'] = "equisD"
 Dic['z'] = "Zeta"
+
 del Dic['y']
+# X = Dic.pop('y') 
+# print(X)
 
+print('x' in Dic)
 
-X = Dic.pop('y', "No existe")
-print(X)                     
+llaves = Dic.keys()
+print(llaves)
 
-print('x' in Dic)            
-print(Dic.keys())            
-print(Dic.items())           
+Valores = Dic.values()
+P = Dic.items()
+
 Dic.clear()
-Dic.update({'x': "x"})
+Dic.update({'x': 'x'})  
 
 print(Dic.get('x', "equis")) 
+
